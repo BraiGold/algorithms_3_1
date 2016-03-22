54ac955aeb3793a325868bf75873c6fdc8b818e7
Lucía Rabinowicz <lu.rabinowicz@gmail.com>
2016-03-22 16:59:01 -0300

    parte de kaioken

---
+++ src/kaioken.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>      /* printf */
+#include <math.h>       /* log */
+
+void kaioken (int cantGuerreros) {
+	int cantMinPeleas = log2(cantGuerreros);
+	cantMinPeleas = ceil(cantMinPeleas);
+}
