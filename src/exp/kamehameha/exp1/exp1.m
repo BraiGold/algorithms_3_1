d936e4e407b20fddbe633734a3bd22f04b7bb42d
martinezmanuela <martinez.manuela.22@gmail.com>
2016-04-29 11:58:41 -0300

    cambio grafico exp1 kamehameha con nueva complejidad

--- src/exp/kamehameha/exp1/exp1.m
+++ src/exp/kamehameha/exp1/exp1.m
@@ -14,9 +14,10 @@ primero = enemigos_x(1);
 
 
 eje_x = primero:ultimo;
-aux = eje_x+2;
-pow = power(eje_x,aux);
-eje_y = times(1/10000000000,pow);
+cuadrado = times(eje_x,eje_x);
+fact = factorial(eje_x);
+aux = times(fact,cuadrado);
+eje_y = times(1/100000000,aux);
 
 
 hold on;
@@ -27,7 +28,7 @@ plot(eje_x,eje_y,'r'); %O(n * (n+2))
 h=errorbar(enemigos_x, tiempo_y, e);
 set(get(h, 'Parent'), 'YScale', 'log');
 hold off;
-legend('Complejidad O(n * (n+2))','Tiempo de ejecucion del algoritmo','Location','northwest')
+legend('Complejidad O(n! * (n*n))','Tiempo de ejecucion del algoritmo','Location','northwest')
 xlabel('Cantidad de Enemigos','FontSize',12);
 ylabel('Tiempo de ejecucion en segundos','FontSize',10);
 title('Tiempo de ejecucion en funcion de la cantidad de enemigos');
